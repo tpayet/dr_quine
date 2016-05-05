f051aaf997297fae038fb97b6210b00ca1b1b892
Thomas PAYET <tpayet@student.42.fr>
2016-05-05 19:04:12 +0200

    adding 02_python

---
+++ Grace.py
@@ -0,0 +1,3 @@
+s = "s = {:s}f = open('Grace_kid.py', 'w');f.write(s.format(chr(34)+s+chr(34)+chr(10), chr(10), chr(10))); f.close{:s}# comment{:s}"
+f = open('Grace_kid.py', 'w');f.write(s.format(chr(34)+s+chr(34)+chr(10), chr(10), chr(10))); f.close
+# comment
