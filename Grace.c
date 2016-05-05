1f70c30cae3d7c264f70f0d2c54faed4e8b7b719
Thomas PAYET <tpayet@student.42.fr>
2016-05-05 18:27:06 +0200

    workin 02 in c

--- Grace.c
+++ Grace.c
@@ -2,8 +2,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#define STRING "#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c#include <string.h>%c#define STRING %c%s%c%c#define WRITE fprintf(f, STRING, 10, 10, 10, 10,  34, STRING, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10)%c#define FT int main(){FILE *f=fopen(%cGrace_kid.c%c, %cw%c); WRITE; fclose(f);}%c%cFT%c"
-#define WRITE fprintf(f, STRING, 10, 10, 10, 10,  34, STRING, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10)
+#define STRING "#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c#include <string.h>%c#define STRING %c%s%c%c#define WRITE fprintf(f, STRING, 10, 10, 10, 10,  34, STRING, 34, 10, 10, 34, 34, 34, 34, 10, 10, 9, 10, 10, 10)%c#define FT int main(){FILE *f=fopen(%cGrace_kid.c%c, %cw%c); WRITE; fclose(f);}%c/*%c%ccomment%c*/%cFT%c"
+#define WRITE fprintf(f, STRING, 10, 10, 10, 10,  34, STRING, 34, 10, 10, 34, 34, 34, 34, 10, 10, 9, 10, 10, 10)
 #define FT int main(){FILE *f=fopen("Grace_kid.c", "w"); WRITE; fclose(f);}
-
+/*
+	comment
+*/
 FT
