6d43f7eb7fbb32384aacc0ecbedb6a5907df1177
Thomas PAYET <tpayet@e2r5p4.42.fr>
2016-05-05 13:02:23 +0200

    adding first working quine programe, need improvement for norminette u_u

---
+++ Colleen.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int main(void)
+{
+char *str = "#include <stdio.h>%cint main(void)%c{%cchar *str = %c%s%c;printf(str, 10, 10, 10, 34, str, 34);}";printf(str, 10, 10, 10, 34, str, 34);}
