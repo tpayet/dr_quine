366ace3d0de90541a0bb9fb4aed90fd4a4a6f419
Thomas PAYET <tpayet@student.42.fr>
2016-05-05 16:03:50 +0200

    adding comment in ex01 in C, working

--- Colleen.c
+++ Colleen.c
@@ -2,8 +2,14 @@
 
 int	main(void)
 {
+	/*
+		first comment in main
+	*/
 	char *str;
 
-	str = "#include <stdio.h>%c%cint%cmain(void)%c{%c%cchar *str;%c%c%cstr = %c%s%c;%c%cprintf(str, 10, 10, 9, 10, 10, 9, 10, 10, 9, 34, str, 34, 10, 9, 10, 10);%c}%c";
-	printf(str, 10, 10, 9, 10, 10, 9, 10, 10, 9, 34, str, 34, 10, 9, 10, 10);
+	str = "#include <stdio.h>%c%cint%cmain(void)%c{%c%c/*%c%c%cfirst comment in main%c%c*/%c%cchar *str;%c%c%cstr = %c%s%c;%c%cprintf(str, 10, 10, 9, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10, 9, 34, str, 34, 10, 9, 10, 10, 10, 9, 10, 10);%c}%c/*%c%csecond comment out of main%c*/%c";
+	printf(str, 10, 10, 9, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10, 9, 34, str, 34, 10, 9, 10, 10, 10, 9, 10, 10);
 }
+/*
+	second comment out of main
+*/
