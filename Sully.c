6b07d8405554dc6d936849d4b8ef9a9f68849c6b
Thomas Payet <tpayet@student.42.fr>
2016-05-06 13:18:55 +0200

    Working Sully.c"

--- Sully.c
+++ Sully.c
@@ -1,10 +1,10 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define STRING "#include <fcntl.h>%c#include <stdio.h>%c#include <stdlib.h>%c#define STRING %c%s%c%c#define N %c%i%c%c#define NEXT %cSully_%c N %c.c%c%c#define WRITE fprintf(f, STRING, 10, 10, 10, 34, STRING, 34, 10, 34, atoi(N)-1, 34, 10, 34, 34, 34, 34, 10, 10, 34, 34, 10, 10)%c#define FT int main(){FILE *f=fopen(NEXT, %cw%c); WRITE; fclose(f);}%cFT%c"
-#define N "5"
-#define NEXT "Sully_" N ".c"
-#define WRITE fprintf(f, STRING, 10, 10, 10, 34, STRING, 34, 10, 34, atoi(N)-1, 34, 10, 34, 34, 34, 34, 10, 10, 34, 34, 10, 10)
-#define CMD "clang -Wall -Wextra -Werror"
-#define FT int main(){FILE *f=fopen(NEXT, "w"); WRITE; fclose(f);}
-FT
+#define STRING "#include <fcntl.h>%c#include <stdio.h>%c#include <stdlib.h>%c#define STRING %c%s%c%cint main(){%cint i=%i;%cif(i>0){char namefile[30];sprintf(namefile, %cSully_%ci.c%c, i);%cchar str[1000];sprintf(str, STRING, 10, 10, 10, 34, STRING, 34, 10, 10, i-1, 10, 34, 37, 34, 10, 10, 34, 37, 34, 10, 34, 34, 10);%cchar cmd[90]; sprintf(cmd, %cclang -Wall -Wextra -Werror Sully_%ci.c && ./a.out%c, i);%cFILE *f=fopen(namefile,%cw%c);fputs(str, f);fclose(f);system(cmd);}}%c"
+int main(){
+int i=5;
+if(i>0){char namefile[30];sprintf(namefile, "Sully_%i.c", i);
+char str[1000];sprintf(str, STRING, 10, 10, 10, 34, STRING, 34, 10, 10, i-1, 10, 34, 37, 34, 10, 10, 34, 37, 34, 10, 34, 34, 10);
+char cmd[90]; sprintf(cmd, "clang -Wall -Wextra -Werror Sully_%i.c && ./a.out", i);
+FILE *f=fopen(namefile,"w");fputs(str, f);fclose(f);system(cmd);}}
